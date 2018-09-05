7f5ac7aba5728791433373d86ea409f6afb8b340
Sebastian Guesdon <sebastian.guesdon@nr-communication.fr>
2018-09-05 21:16:41 +0200

    reprise header

--- header.php
+++ header.php
@@ -3,31 +3,31 @@
 <html class="no-js" <?php language_attributes(); ?>>
 
 	<head profile="http://gmpg.org/xfn/11">
-		
+
 		<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >
-		 
+
 		<?php wp_head(); ?>
-	
+
 	</head>
-	
+
 	<body <?php body_class(); ?>>
-		
+
 		<div class="navigation">
-			
+
 			<div class="section-inner">
-				
+
 				<ul class="main-menu">
-																		
-					<?php 
+
+					<?php
 					if ( has_nav_menu( 'primary' ) ) {
 
-						$nav_args = array( 
+						$nav_args = array(
 							'container' 		=> '',
 							'items_wrap' 		=> '%3$s',
 							'theme_location' 	=> 'primary',
 						);
-																		
+
 						wp_nav_menu( $nav_args );
 
 					} else {
@@ -39,85 +39,74 @@
 
 						wp_list_pages( $list_pages_args );
 
-					} 
+					}
 					?>
-					
+
 					<li class="header-search">
 						<form method="get" class="search-form" id="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-							<input type="search" class="search-field" name="s" placeholder="<?php _e( 'Search Form', 'hitchcock' ); ?>" /> 
+							<input type="search" class="search-field" name="s" placeholder="<?php _e( 'Search Form', 'hitchcock' ); ?>" />
 							<a class="search-button" onclick="document.getElementById( 'search-form' ).submit(); return false;"><div class="fa fw fa-search"></div></a>
 						</form>
 					</li>
-					
+
 				</ul>
-				
+
 				<div class="clear"></div>
-				
+
 			</div><!-- .section-inner -->
-			
+
 			<div class="nav-toggle">
-					
+
 				<div class="bars">
 					<div class="bar"></div>
 					<div class="bar"></div>
 					<div class="bar"></div>
 				</div>
-				
+
 			</div><!-- .nav-toggle -->
-			
+
 			<div class="mobile-navigation">
-			
+
 				<ul class="mobile-menu">
-																			
-					<?php 
+
+					<?php
 					if ( has_nav_menu( 'primary' ) ) {
 						wp_nav_menu( $nav_args );
 					} else {
 						wp_list_pages( $list_pages_args );
 					}
 					?>
-					
+
 				</ul>
-				
+
 				<?php get_search_form(); ?>
-			
+
 			</div><!-- .mobile-navigation -->
-			
+
 		</div><!-- .navigation -->
 
 		<?php $image_image_url = get_header_image() ? get_header_image() : get_template_directory_uri() . '/images/bg.jpg'; ?>
-		
-		<div class="header-image" style="background-image: url( <?php echo $image_image_url; ?> );"></div>
-	
-		<div class="header section-inner">
-		
-			<?php 
-			
-			if ( get_theme_mod( 'custom_logo' ) ) :
 
-				hitchcock_custom_logo();
-				
-			else : 
-				
-				$title_type = is_singular() ? '2' : '1'; ?>
-		
-				<h<?php echo $title_type; ?> class="blog-title">
-					<a href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( get_bloginfo( 'title' ) ); ?> &mdash; <?php echo esc_attr( get_bloginfo( 'description' ) ); ?>" rel="home"><?php echo esc_attr( get_bloginfo( 'title' ) ); ?></a>
-				</h<?php echo $title_type; ?>>
-				
-			<?php endif;
-			
-			if ( get_bloginfo( 'description' ) ) : ?>
-			
-				<p class="blog-description"><?php echo bloginfo( 'description' ); ?></p>
-			
-			<?php endif;
-			
-			if ( has_nav_menu( 'social' ) ) : ?>
-			
+		<div class="slider">
+			<div class="slider-header">
+				LOGO
+			</div>
+			<figure>
+				<img src="/wp-content/uploads/2018/09/drone3.jpg" alt>
+				<img src="/wp-content/uploads/2018/09/drone3.jpg" alt>
+				<img src="/wp-content/uploads/2018/09/drone3.jpg" alt>
+				<img src="/wp-content/uploads/2018/09/drone3.jpg" alt>
+				<img src="/wp-content/uploads/2018/09/drone3.jpg" alt>
+			</figure>
+		</div>
+
+		<div class="header-image" style="background-image: url( <?php echo $image_image_url; ?> );"></div>
+
+		<?php if ( has_nav_menu( 'social' ) ) : ?>
+		<div class="header section-inner">
+			<?php if ( has_nav_menu( 'social' ) ) : ?>
 				<ul class="social-menu">
-							
-					<?php 
+					<?php
 					wp_nav_menu( array(
 						'theme_location'	=>	'social',
 						'container'			=>	'',
@@ -131,9 +120,7 @@
 						'fallback_cb'		=>	'',
 					) );
 					?>
-					
 				</ul><!-- .social-menu -->
-			
 			<?php endif; ?>
-			
 		</div><!-- .header -->
+		<?php endif; ?>
